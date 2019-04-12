6099e6848bec4aec045360e258f99d34bd7ec71d
Jason W Ellsworth <jasonellsworth48@gmail.com>
2019-04-12 21:41:53 +0800

    Add files via upload

---
+++ Stalkerbit/stalker.js
@@ -0,0 +1,11 @@
+//Ask for basic info
+
+let userFirstName = prompt("What is your first name?");
+let userLastName = prompt("What is your last name?");
+let userAge = prompt("What is your age?");
+
+//concat info to read out
+
+alert(`your full name is ${userFirstName} ${userLastName}`);
+
+alert(`your age is ${userAge}`);
